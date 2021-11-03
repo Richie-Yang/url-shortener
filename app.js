e5011fd62e0aff58c43dd061eb5003f2a7fe7abe
Richie-Yang <richieyang8813@gmail.com>
2021-11-03 09:58:50 +0800

    feat: add Handlebars and index + main pages

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
+// Initialize Express framework
 const app = express()
 const port = 3000
 
+// Set template engine to Handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+
+////// Routing Section Starts Here //////
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.render('index')
 })
+////// Routing Section Ends Here //////
 
 
+// Start Express server 
 app.listen(port, () => {
   console.log(`Express server is listening on 127.0.0.1:${port}`)
 })
