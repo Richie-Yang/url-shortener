995f2fc28cf9b3b638d5f6ec535659bde40c8a99
Richie-Yang <richieyang8813@gmail.com>
2021-11-03 10:07:00 +0800

    feat: add body-parser to process POST request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 // Initialize Express framework
 const app = express()
@@ -9,9 +10,16 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 ////// Routing Section Starts Here //////
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body.url)
   res.render('index')
 })
 ////// Routing Section Ends Here //////
