fadebe1c0ecf7b545113f8d59bdf6568b12d618e
Richie-Yang <richieyang8813@gmail.com>
2021-11-04 14:32:40 +0800

    refactor: modfiy POST/ function logic

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -31,29 +31,53 @@ router.post('/', (req, res) => {
   const original_URL = req.body.url.trim()
   if (!original_URL.length) res.redirect('/')
 
-  // first check if original_URL already exists
-  return URL.findOne({ original_URL })
-    .then(url => {
-      // if not exist, following script will be run
-      if (!url) {
+  // New code (2021/11/4)
+  return URL.find()
+    .then(urls => {
+      const oldURL = urls.filter(item => item.original_URL === original_URL)[0]
+
+      // if undefined (oldURL not found), following script will be run
+      if (!oldURL) {
         const host = req.headers.host
         let redirect_index = generateRandomIndex()
 
         // trying to do loop check for redirect_index until no duplication found
-        URL.find()
-          .then(urls => {
-            while (urls.some(item => item.redirect_index === redirect_index)) {
-              redirect_index = generateRandomIndex()
-            }
-            const shortened_URL = `http://${host}/${redirect_index}`
-            return new URL({ original_URL, shortened_URL, redirect_index }).save()
-          })
+        while (urls.some(item => item.redirect_index === redirect_index)) {
+          redirect_index = generateRandomIndex()
+        }
+        const shortened_URL = `http://${host}/${redirect_index}`
+        URL.create({ original_URL, shortened_URL, redirect_index })
           .then(newURL => res.redirect(`/urls/${newURL.id}`)) // redirect
           .catch(error => console.log(error))
-
-      } else res.redirect(`/urls/${url.id}`) // if exist, then redirect
+      } else res.redirect(`/urls/${oldURL.id}`) // if defined, then redirect
     })
     .catch(error => console.log(error))
+
+
+  // Old code (2021/11/3)
+  // // first check if original_URL already exists
+  // return URL.findOne({ original_URL })
+  //   .then(url => {
+  //     // if not exist, following script will be run
+  //     if (!url) {
+  //       const host = req.headers.host
+  //       let redirect_index = generateRandomIndex()
+
+  //       // trying to do loop check for redirect_index until no duplication found
+  //       URL.find()
+  //         .then(urls => {
+  //           while (urls.some(item => item.redirect_index === redirect_index)) {
+  //             redirect_index = generateRandomIndex()
+  //           }
+  //           const shortened_URL = `http://${host}/${redirect_index}`
+  //           return new URL({ original_URL, shortened_URL, redirect_index }).save()
+  //         })
+  //         .then(newURL => res.redirect(`/urls/${newURL.id}`)) // redirect
+  //         .catch(error => console.log(error))
+
+  //     } else res.redirect(`/urls/${url.id}`) // if exist, then redirect
+  //   })
+  //   .catch(error => console.log(error))
 })
 ////// Routing Section Ends Here //////
 
